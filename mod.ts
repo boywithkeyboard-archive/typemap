2420c69b1d5cf90aca519ac5a4806004c0ef8ca6
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-10 17:18:20 +0200

    refactor: rewrite entry file

--- mod.ts
+++ mod.ts
@@ -1,76 +1,44 @@
-import {
-  Any,
-  Array,
-  Blob,
-  Boolean,
-  Buffer,
-  Date,
-  FormData,
-  Literal,
-  Null,
-  Number,
-  Object,
-  ObjectId,
-  Omit,
-  Optional,
-  Partial,
-  Pick,
-  Record,
-  Recursive,
-  RegEx,
-  Required,
-  Stream,
-  String,
-  Tuple,
-  Undefined,
-  Union,
-  Unknown,
-} from './types.ts'
+import { Type, Value, Static, TSchema } from './deps.ts'
+import { blobType } from './types/blob.ts'
+import { bufferType } from './types/buffer.ts'
+import { formDataType } from './types/formData.ts'
+import { objectIdType } from './types/objectId.ts'
+import { streamType } from './types/stream.ts'
 
-export default {
-  // types
-  any: Any,
-  array: Array,
-  blob: Blob,
-  boolean: Boolean,
-  buffer: Buffer,
-  date: Date,
-  formData: FormData,
-  literal: Literal,
-  null: Null,
-  number: Number,
-  object: Object,
-  objectId: ObjectId,
-  regex: RegEx,
-  stream: Stream,
-  string: String,
-  undefined: Undefined,
-  unknown: Unknown,
+const validate = Value.Check as <T extends TSchema>(schema: T, value: unknown) => value is Static<T>
 
-  // modifiers
-  omit: Omit,
-  optional: Optional,
-  partial: Partial,
-  pick: Pick,
-  record: Record,
-  recursive: Recursive,
-  required: Required,
-  tuple: Tuple,
-  union: Union,
+const types = {
+  // standard
+  any: Type.Any.bind(Type),
+  array: Type.Array.bind(Type),
+  bigint: Type.BigInt.bind(Type),
+  boolean: Type.Boolean.bind(Type),
+  date: Type.Date.bind(Type),
+  literal: Type.Literal.bind(Type),
+  null: Type.Null.bind(Type),
+  number: Type.Number.bind(Type),
+  object: Type.Object.bind(Type),
+  omit: Type.Omit.bind(Type),
+  optional: Type.Optional.bind(Type),
+  partial: Type.Partial.bind(Type),
+  pick: Type.Pick.bind(Type),
+  record: Type.Record.bind(Type),
+  regex: Type.RegEx.bind(Type),
+  required: Type.Required.bind(Type),
+  string: Type.String.bind(Type),
+  tuple: Type.Tuple.bind(Type),
+  undefined: Type.Undefined.bind(Type),
+  union: Type.Union.bind(Type),
+  unknown: Type.Unknown.bind(Type),
+
+  // custom
+  blob: blobType,
+  buffer: bufferType,
+  formData: formDataType,
+  objectId: objectIdType,
+  stream: streamType
 }
 
-import { Value } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.25.24/value/value.js/+esm'
-import type {
-  Static,
-  TSchema,
-} from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.25.24/typebox.d.ts'
+export const v = Object.assign(validate, types)
 
-export const isValid = Value.Check as <T extends TSchema>(
-  schema: T,
-  value: unknown,
-) => value is Static<T>
-
-export type {
-  Static,
-  TSchema as TypeSchema,
-} from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.25.24/typebox.d.ts'
+export type { Static, TSchema as Schema } from './deps.ts'
