822a9b413ef960678be343e6083c9bed7b436007
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-04-10 17:18:09 +0200

    chore: add deps file

---
+++ deps.ts
@@ -0,0 +1,6 @@
+export { Value } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.1/value/value.js/+esm'
+export { Type } from 'https://cdn.jsdelivr.net/gh/sinclairzx81/typebox@0.27.1/src/typebox.ts'
+export type { Static, TSchema } from 'https://cdn.jsdelivr.net/gh/sinclairzx81/typebox@0.27.1/src/typebox.ts'
+// @deno-types='https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.1/system/system.d.ts'
+export { TypeSystem } from 'https://cdn.jsdelivr.net/npm/@sinclair/typebox@0.27.1/system/system.js/+esm'
+export { ObjectId } from 'https://deno.land/x/realm@v0.1.1/mod.ts'
